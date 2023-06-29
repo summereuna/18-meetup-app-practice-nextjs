a0fab4ca9325a6237f699b7e27775bd37a6e90f3
summereuna <summereuna@gmail.com>
2023-06-29 14:30:49 +0900

    Feat programming way to navigate in detailpage

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,6 @@
 // 도메인.com/new-meetup/:meetupId
 import { useRouter } from "next/router";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 export default function meetUpDetailPage() {
   const router = useRouter();
@@ -10,8 +11,11 @@ export default function meetUpDetailPage() {
 
   console.log(meetupId);
   return (
-    <>
-      <h1>meetUpDetailPage</h1>
-    </>
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1526199119161-4be1e3368d52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80"
+      title="test 1"
+      address="📍 삶은 여행, 나중에 카카오맵 api 넣어야지"
+      description="삶은 여행"
+    />
   );
 }
