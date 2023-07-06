31c8505455725da28cbcd6ed72dbdbf026f8d5e9
summereuna <summereuna@gmail.com>
2023-07-06 19:13:26 +0900

    Feat connect MongoDB

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,31 @@
 // 도메인.com/new-meetup
-//여긴 데이터 페치 필요 없음
 
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
-export default function newMeetupPage() {
-  const addMeetupHandler = (meetupData) => {
-    console.log(meetupData);
-    //백엔드에 넘기기
+function newMeetupPage() {
+  const router = useRouter();
+
+  //meetupData 받아서
+  const addMeetupHandler = async (meetupData) => {
+    //🔥 백엔드에 넘기기
+    // 🔥 /api/new-meetup.js 파일로 요청 전송하고 거기에 있는 함수 트리거함
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json", //JSON 데이터 보내는 것 명확히 하기 위해 작성
+      },
+    });
+
+    //응답 받으면 데이터 얻음
+    const data = await response.json();
+    // console.log(data);
+
+    //폼 입력 끝나면 다른 페이지로 이동
+    router.push("/");
   };
+
   return (
     <>
       <h1>newMeetupPage</h1>
@@ -15,3 +33,5 @@ export default function newMeetupPage() {
     </>
   );
 }
+
+export default newMeetupPage;
